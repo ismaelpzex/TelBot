86b3c18f9f712f019c4345f159bd8492a10fa32c
Ismael de la Poza <ismaelpzex@gmail.com>
2022-08-10 09:32:43 +0200

    add basic configuration

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const express = require('express');
+const { Telegraf } = require('telegraf');
+
+//Load .env
+require('dotenv').config();
+
+//Create app
 const app = express();
+
+//Create bot instance
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+//Bot config
+app.use(bot.webhookCallback('/url_telegram'));
+bot.telegram.setWebhook(`${process.env.BOT_URL}/url_telegram`);
+
+//Telegram reqs
+app.post('/url_telegram', (req, res) => {
+    res.status(200).send('Fin de la petición');
+});
+
+//Config PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Servidor escuchando en puerto ' + PORT);
+});
